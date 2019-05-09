9bf72c9401b7d2e224a522adfcee138c4d5def11
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-05-09 18:02:39 +0800

    9.斐波那契数列.java
    一个斐波那契数列是由数字1，1，2，3，5，8，13等等组成，其中每一个数字（从第三个数字起）都是前两个数字的和。创建一个方法，接受一个整数参数，并显示从第一个元素开始总共由该参数指定的个数所构成是我所有斐波那契数字。

---
+++ 控制执行流程/9.斐波那契数字.java
@@ -0,0 +1,15 @@
+public class Code_练习9 {
+    public static void main(String[] args) {
+        int k = 5;
+        for (int i = 1;i <= 5;i++){
+            System.out.println(fib(i));
+        }
+    }
+
+    public static int fib(int v) {
+        if (v <= 2) {
+            return 1;
+        }
+        return fib(v - 1) + fib(v - 2);
+    }
+}
