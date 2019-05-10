5f57b668520360ea46e61857197a296d921c1848
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-05-10 20:46:48 +0800

    18.创建数组对象.java

---
+++ 初始化与清理/18.创建数组对象.java
@@ -0,0 +1,17 @@
+class Person1{
+    Person1(String s){
+        System.out.println(s);
+    }
+}
+public class Code_练习17和18 {
+    public static void main(String[] args) {
+        Person1[] person = {
+                new Person1("SUSAN"),
+                new Person1("JACK"),
+                new Person1("alice")
+        };
+        for (int i = 0; i < person.length;i++){
+            System.out.println(person[i]);
+        }
+    }
+}
