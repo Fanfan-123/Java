4892c726f247c40913ea04b904c17c5103572a16
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-05-24 23:30:16 +0800

    Create 字符串中找出连续最长的数字串.java

---
+++ 练习/字符串中找出连续最长的数字串.java
@@ -0,0 +1,35 @@
+package com.kelly;
+
+import java.util.Stack;
+
+public class day3_2 {
+    public boolean chkParenthesis(String A, int n) {
+        char[] c = A.toCharArray();
+        Stack<Character>  left = new Stack<Character>();
+        int count = 0;
+
+        for (int i = 0; i < c.length;i++){
+            if (c[i] == '('){
+                left.push(c[i]);
+                count++;
+            }
+            if (c[i] == ')'){
+                if (left.empty()){
+                    return false;
+                }else {
+                    count--;
+                }
+            }
+        }
+        if (count == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        day3_2 d = new day3_2();
+        d.chkParenthesis("())",3);
+    }
+}
