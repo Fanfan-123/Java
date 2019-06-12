7db840e7bdcfad1cbb1ff4c692e3f28144a14be2
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-06-12 19:18:32 +0800

    构造方法.java

---
+++ 练习/构造方法.java
@@ -0,0 +1,36 @@
+ class Person2{
+ 
+	 //属性——private封装
+	private String name;
+	private int age;
+	//对属性的 setter设置 和 getter获取
+	public void setName(String n){
+		name = n;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setAge(int a){
+		if(age < 0 || age >= 150){
+			System.out.println("输入错误！");
+		}
+		else{
+		age = a;
+		}
+	}
+	public int getAge(){
+		return age;
+	}
+	
+	//构造方法
+	public Person2(String n,int a){
+		name = n;
+		setAge(a);
+	}
+	
+	//方法
+	public void intriduce(){
+		System.out.println("姓名："+name+"，年龄："+age);
+	}
+}
+
