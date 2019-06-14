6774c037e250938241b93783a9cb0cdb9da67d22
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-06-14 21:08:35 +0800

    匿名数组.java

---
+++ 练习/匿名数组.java
@@ -0,0 +1,6 @@
+public class ArrayDemo{
+    public static void main(String[] args) {        
+    System.out.println(new int[]{1,2,5,55,555,223,45545,666465,6443}.length) ;
+     // 此处为匿名数组    
+    }
+  }
