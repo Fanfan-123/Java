b34323e5a7c71adac4ced0cf8457b05c0042ab2a
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-06-14 21:09:09 +0800

    二维数组.java

---
+++ 练习/二维数组.java
@@ -0,0 +1,16 @@
+public class ArrayDemo{
+    public static void main(String[] args) {
+ 
+       int[][] data = new int[][] { 
+                   {1,2,3},{4,5},{6,7,8,9}
+                };
+                       
+ // 在进行输出的时候一定要使用双重循环
+         for (int x = 0; x<data.length ; x++) {
+              for (int y = 0; y<data[x].length ;y ++) {
+            System.out.println("data{"+x+"]["+y+"]="+data[x][y]+"、") ;   
+                     }   
+            System.out.println();
+              }
+              } 
+            }
