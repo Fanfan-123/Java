8308c58642d68f64fae356844c4762bcc804f95e
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-06-14 21:07:20 +0800

    数组动态初始化.java

---
+++ 练习/数组动态初始化.java
@@ -0,0 +1,12 @@
+public class ArrayDemo{
+    public static void main(String[] args) {
+       int[] x = new int[3] ;         // 开辟了一个长度为3的数组           
+       System.out.println(x.length) ;
+       x[0] = 1 ;                    // 数组第一个元素 
+       x[1] = 2 ;                   // 数组第二个元素 
+       x[2] = 3 ;                   // 数组第三个元素
+       for (int i = 0; i<x.length ; i++) {            
+       System.out.println(x[i]) ; // 通过循环控制索引下标更改    
+           } 
+          } 
+          }
