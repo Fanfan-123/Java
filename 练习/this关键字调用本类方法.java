b17589a7cd6b11e5e643cc2cc5303b5a42f3a932
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-06-18 22:42:58 +0800

    this关键字调用本类方法.java

---
+++ 练习/this关键字调用本类方法.java
@@ -0,0 +1,19 @@
+class Person2{
+	//方法
+	private String name;
+	//构造方法
+	//参数名（即局部变量）与属性名（即成员变量名）相同，
+	//为了加以区分，属性名前加 this，用于访问属性变量
+	public Person2(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+		//此处调用普通方法，但是一定要在本类中
+		this.print();
+	}	
+	//普通方法
+	public void print(){
+		System.out.prinbtln("##33##");
+	}
+}
