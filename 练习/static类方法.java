af916c9036d1bece08bde7957aa43646c811b887
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-06-19 22:49:16 +0800

    static类方法.java

---
+++ 练习/static类方法.java
@@ -0,0 +1,20 @@
+class Person2{
+	 //此处为了强调static关键字的作用，设为公共属性
+	public static String country = "中国";
+	public String name;
+	public int age;
+	//构造方法
+	public Person2(String name,int age){
+		this.name = name;
+		this.age = age;
+	}
+	//方法
+	public static void print(){
+		System.out.println("hehehe");
+	}
+	public void intriduce(){
+		Person2.country = "韩国";
+		System.out.println("姓名："+name+"，年龄："+age+",国家："+country);
+	}
+}
+
