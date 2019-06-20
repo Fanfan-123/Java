34ee5c52f9c74ff570c05d0acc5adeecdf0e7806
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-06-20 08:51:20 +0800

    代码块—主类中的静态块.java

---
+++ 练习/代码块—主类中的静态块.java
@@ -0,0 +1,19 @@
+public class Example1{
+	//主类中的静态代码块
+	static {
+		System.out.println("0,主类下的静态代码块");
+	}
+	//构造块
+	{
+		System.out.println("1,类下的构造块");
+	}
+	//构造方法
+	public Example1(){
+		System.out.println("2,类下的构造方法");
+	}
+	public static void main(String[] args){
+		//new Example1();
+		//System.out.println("——————-————————————");
+		//new Example1();
+	}
+}
