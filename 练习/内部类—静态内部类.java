d78e2e2633bbcae3033d07430ec456aae4430fda
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-06-22 18:51:00 +0800

    内部类—静态内部类.java

--- 练习/内部类—静态内部类.java
+++ 练习/内部类—静态内部类.java
@@ -2,7 +2,6 @@ class Outer{
 	private static String name = "jack";
 	static class Inner{
 		public void print(){
-		
 			//静态内部类使用外部类的实行时，只能调用外部类的static属性
 			//此时不能用 （属性名.this.属性名）形式，直接使用属性名即可
 			// System.out.println("姓名为："+Outer.this.name);---error
