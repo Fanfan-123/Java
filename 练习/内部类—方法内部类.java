14dd6f735a905d446709b2b47e6265f3df48d06a
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-06-22 18:49:10 +0800

    内部类—方法内部类.java

---
+++ 练习/内部类—方法内部类.java
@@ -0,0 +1,18 @@
+class Outer{
+	private String name = "Alice";
+	//方法内部类定义在外部类方法中，只能在该方法中使用
+	public void display(){
+		class Inner{
+			public void print(){
+				System.out.println(name+" is beautiful!!!");
+		}
+		}
+		new Inner().print();//创建匿名对象并调用方法内部类中的方法
+	}
+}
+public class TestClass{
+	public static void main(String[] args){
+	Outer out = new Outer();
+	out.display();
+	}
+}
