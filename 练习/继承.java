3a4869d9a8b86c5e204bd30ee7f4a93d643954e1
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-06-23 18:41:04 +0800

    继承.java

---
+++ 练习/继承.java
@@ -0,0 +1,21 @@
+//主类
+public class TestExtends{
+	public static void main(String[] args){
+        Student stu = new Student();
+	    stu.name = "kelly";
+	    stu.age = 23;
+	    stu.print();		
+	}
+}
+//父类 
+class Person{
+    String name;
+	int age;
+	public void print(){
+		System.out.println("姓名："+name+",年龄："+age);
+	}	
+}
+//子类
+class Student extends Person{
+	
+}
