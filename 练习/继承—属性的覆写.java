24c1eff1243bbd57caf15c9ee6f55ebb84d1b6cb
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-06-26 22:32:41 +0800

    Create 继承—属性的覆写.java

---
+++ 练习/继承—属性的覆写.java
@@ -0,0 +1,21 @@
+public class TestFatherAndSon{
+	public static void main(String[] args){
+		Student stu = new Student();
+		System.out.println(stu.getName());
+	}
+}
+
+
+class Person{
+	//父类中的属性不为private时，子类才能覆写父类中的属性
+	public String name = "kelly";
+}
+
+
+class Student extends Person{
+	//属性覆写，不需要范围更为宽泛
+	private String name = "alice"; 
+	public String getName(){
+		return name;
+	}
+}
