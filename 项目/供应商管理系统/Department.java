ee8b5be1fbf4b1b11ed41ce53b2ff56d0706823e
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-07-08 22:07:33 +0800

    Department.java
    定义供应商管理系统的各个属性

---
+++ 项目/供应商管理系统/Department.java
@@ -0,0 +1,43 @@
+package danfeng.domain;
+
+public class Department {
+	private int D_id;
+	private String D_name;
+	private String D_admin;
+	
+	public Department(){
+		
+	}
+	
+	public Department(int d_id, String d_name, String d_admin) {
+		super();
+		D_id = d_id;
+		D_name = d_name;
+		D_admin = d_admin;
+	}
+
+	public int getD_id() {
+		return D_id;
+	}
+	public void setD_id(int d_id) {
+		D_id = d_id;
+	}
+	public String getD_name() {
+		return D_name;
+	}
+	public void setD_name(String d_name) {
+		D_name = d_name;
+	}
+	public String getD_admin() {
+		return D_admin;
+	}
+	public void setD_admin(String d_admin) {
+		D_admin = d_admin;
+	}
+	@Override
+	public String toString() {
+		return "Department [D_id=" + D_id + ", D_name=" + D_name + ", D_admin=" + D_admin + "]";
+	}
+	
+	
+}
