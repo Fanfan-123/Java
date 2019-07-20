bc5ec1250305a99263195769811a36711d000de1
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-07-20 23:15:02 +0800

    覆写toString()方法.java

---
+++ Object类/覆写toString()方法.java
@@ -0,0 +1,21 @@
+class Person{
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String toString(){
+        return "姓名："+this.name+",年龄："+this.age;
+    }
+}
+public class Object_Person {
+    public static void main(String[] args) {
+        fun(new Person("alice",20));
+    }
+    public static void fun(Object obj){
+        System.out.println(obj.toString());
+    }
+}
+
