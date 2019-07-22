7f00e000e3a5a26a1b899cc1b271be030daf7979
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-07-22 22:19:47 +0800

    对象在不同位置的初始化.java

---
+++ 初始化与清理/对象在不同位置的初始化.java
@@ -0,0 +1,28 @@
+public class Initialization {
+    private String
+            //1. 在定义对象的时候进行初始化
+    s1 = "happy",
+    s2,
+    s3;
+  public  Initialization(){
+      //2. 在构造方法中进行初始化
+      s2 = "kelly";
+    }
+
+    public String toString(){
+      //3. 惰性初始化
+      if (s3 == null){
+          s3 = "hello";
+      }
+      return "s1:"+s1+"\n"+
+              "s2:"+s2+"\n"+
+              "s3:"+s3+"\n";
+    }
+
+    public static void main(String[] args) {
+      //4. 使用实例初始化
+      Initialization itlize = new Initialization();
+      System.out.println(itlize);
+    }
+
+}
